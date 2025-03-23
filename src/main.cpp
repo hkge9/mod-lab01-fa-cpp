ed919ff1e529a6cabf7f4b5c4e991cb104d51b79
Vladislav Melnikov <ninthofhokage@gmail.com>
2025-03-23 16:26:51 +0300

    update fun.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
+    const char* str1 = "Hello 123 world test 44 hi";
+    const char* str2 = "Hello world H Test Hi tEst";
+    const char* str3 = "This is an example sentence";
+
+    std::cout << "faStr1(\"" << str1 << "\") = " << faStr1(str1) << std::endl;
+    std::cout << "faStr2(\"" << str2 << "\") = " << faStr2(str2) << std::endl;
+    std::cout << "faStr3(\"" << str3 << "\") = " << faStr3(str3) << std::endl;
+
     return 0;
 }
